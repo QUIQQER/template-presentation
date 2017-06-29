70cc0cfbfec6182e832a1126142722f112a771a4
Michael <michael.danielczok@gmail.com>
2017-06-29 17:17:45 +0200

    refactor: init.js umstrukturiert - u.a. Benutzung von QUI Objekt feat: Styling für FAQ Modul

--- bin/init.js
+++ bin/init.js
@@ -1,76 +1,69 @@
 window.addEvent("domready", function () {
     "use strict";
 
-    require(['qui/utils/Functions'], function (QUIFunctionUtils) {
-        if (document.getElements('[href=#top]')) {
-            var toTop = document.getElements('[href=#top]'),
-                hasClass = false;
 
-            // scroll to top
-            toTop.addEvent('click', function (event) {
-                event.stop();
-                new Fx.Scroll(window).toTop();
-            });
-
-            /**
-             * show button toTop after scrolling down
-             */
-            var scroll = function () {
-                if (window.getScroll().y > 300) {
-                    if (!hasClass) {
-                        toTop.addClass('toTop__show');
-                    }
-                    return;
-                }
-                toTop.removeClass('toTop__show');
-                hasClass = false;
-            };
-
-            window.addEvents({
-                'load'  : QUIFunctionUtils.debounce(scroll, 200),
-                'scroll': QUIFunctionUtils.debounce(scroll, 200)
-            })
-
-        }
-    });
-
-// load QUI
+    // load QUI
     require(['qui/QUI'], function (QUI) {
         QUI.addEvent("onError", function (msg, url, linenumber) {
             console.error(msg);
             console.error(url);
             console.error('LineNo: ' + linenumber);
         });
-    });
 
-    /**
-     * show nav background after scroll
-     * works only if nav is position fixed
-     */
-    if (typeof navIsFixed !== 'undefined') {
-        require(['qui/utils/Functions'], function (QUIFunctionUtils) {
-            var headerBar = document.getElement('.header-bar'),
-                hasClass  = false;
+        /**
+         * toTop button
+         */
+        if (document.getElements('[href=#top]')) {
+            var toTop         = document.getElements('[href=#top]'),
+                buttonVisible = false;
 
-            var onScroll = function () {
-                if (window.getScroll().y > 100) {
-                    if (!hasClass) {
-                        headerBar.addClass('header-bar-scrolled');
-                        hasClass = true;
+            // show button toTop after scrolling down
+            QUI.addEvent('scroll', function () {
+                if (QUI.getScroll().y > 300) {
+                    if (!buttonVisible) {
+                        toTop.addClass('toTop__show');
+                        buttonVisible = true;
                     }
-                } else {
-                    headerBar.removeClass('header-bar-scrolled');
-                    hasClass = false;
+                    return;
                 }
-            };set
 
-
-            window.addEvents({
-                'scroll': QUIFunctionUtils.debounce(onScroll, 30),
-                'load'  : QUIFunctionUtils.debounce(onScroll, 30)
+                if (!buttonVisible) {
+                    return;
+                }
+                toTop.removeClass('toTop__show');
+                buttonVisible = false;
             });
-        });
-    }
+
+            // scroll to top
+            toTop.addEvent('click', function (event) {
+                event.stop();
+                new Fx.Scroll(window).toTop();
+            });
+        }
+
+        /**
+         * show nav background after scroll
+         * works only if nav is position fixed
+         */
+        if (typeof navIsFixed !== 'undefined') {
+
+            var headerBar     = document.getElement('.header-bar'),
+                navBackground = false;
+
+            QUI.addEvents('scroll', function () {
+                if (QUI.getScroll().y > 100) {
+                    if (!navBackground) {
+                        headerBar.addClass('header-bar-scrolled');
+                        navBackground = true;
+                    }
+                    return;
+                }
+                headerBar.removeClass('header-bar-scrolled');
+                navBackground = false;
+
+            });
+        }
+    });
 
     /**
      * show the search input after clicking on the icon
